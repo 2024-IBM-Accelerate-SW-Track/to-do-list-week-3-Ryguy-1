612218d8c1930f7ad9c38ec22602739c633bb57b
Ryland Birchmeier <rylandbirchmeier@gmail.com>
2024-06-24 21:27:34 -0400

    all changes

--- src/component/todos.js
+++ src/component/todos.js
@@ -1,6 +1,12 @@
 import React from "react";
 import "../component/todos.css";
-import { Card, Grid, ListItemButton, ListItemText, Checkbox} from "@mui/material";
+import {
+  Card,
+  Grid,
+  ListItemButton,
+  ListItemText,
+  Checkbox,
+} from "@mui/material";
 
 // 1. This component formats and returns the list of todos.
 // 2. Treat the question mark like an if statement.
@@ -12,15 +18,24 @@ import { Card, Grid, ListItemButton, ListItemText, Checkbox} from "@mui/material
 const Todos = ({ todos, deleteTodo }) => {
   const todoList = todos.length ? (
     todos.map((todo) => {
+      const color = new Date(todo.due) < new Date() ? "red" : "white";
       return (
         <Grid key={todo.id}>
-          <Card style={{marginTop:10}}>
+          <Card
+            style={{ marginTop: 10, backgroundColor: color }}
+            data-testid={todo.content}
+          >
             {/* Remember, we set the local state of this todo item when the user submits the form in 
             AddTodo.js. All we need to do is return the todo list item {todo.content} as well as its 
-            current date/time {todo.date}. Also, the item's id is utilized in order to correctly delete an item from the Todo list*/}.
+            current date/time {todo.date}. Also, the item's id is utilized in order to correctly delete an item from the Todo list*/}
+            .
             <ListItemButton component="a" href="#simple-list">
-              <Checkbox style={{paddingLeft:0}} color="primary" onClick={() => deleteTodo(todo.id)}/>
-              <ListItemText primary={todo.content} secondary={todo.date}/>
+              <Checkbox
+                style={{ paddingLeft: 0 }}
+                color="primary"
+                onClick={() => deleteTodo(todo.id)}
+              />
+              <ListItemText primary={todo.content} secondary={todo.due} />
             </ListItemButton>
           </Card>
         </Grid>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -28,8 +28,16 @@ class Home extends Component {
   // the addTodo function simply creates a new array that includes the user submitted todo item and then
   // updates the state with the new list.
   addTodo = (todo) => {
-    const exists = this.state.todos.find(t => t.content === todo.content);
-    if (exists){ return }
+    // check for duplicate todos
+    const exists = this.state.todos.find((t) => t.content === todo.content);
+    if (exists) {
+      return;
+    }
+    // check for invalid date
+    if (todo.due === null || todo.due === "Invalid Date") {
+      return;
+    }
+
     // In React, keys or ids in a list help identify which items have changed, been added or removed. Keys
     // should not share duplicate values.
     // To avoid having dup values, we use the Math.random() function to generate a random value for a todo id.
